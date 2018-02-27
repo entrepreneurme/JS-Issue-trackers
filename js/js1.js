63889fbd234e4259258dc5d5f7b7e6e807d37223
ujjwal khandelwal <ujjwal.khandelwal@yahoo.com>
2018-02-27 16:42:17 +0530

    Add files via upload

---
+++ js/js1.js
@@ -0,0 +1,62 @@
+document.getElementById('issueInputForm').addEventListener('submit',saveIssue);
+function saveIssue(e)
+{
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedTo').value;
+    var issueId = chance.guid();
+    var issueStatus = 'Open';
+
+
+    var issue = {
+        id: isssueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    if(localStorage.getItem('issues')== null){
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues',JSON.stringify(issues));
+    
+
+    }
+    else{
+        var isssues = JSON.parse(localStorage.getItem(issues));
+        issues.push(issue);
+        localStorage.setItem('issues',JSON.stringify(issues));
+
+
+    }
+    document.getElementById('issueInputForm').reset();
+    fetchIssues();
+    e.preventDefault();
+    
+}
+
+function fetchIssues() {
+var issues = JSON.parse(localStorage.getItem('issues'));
+var issuesListe = document.getElementById('issuesList');
+
+issuesList.innerHTML = '';
+
+for(var i = 0; i < issues.length; i++) {
+    var id = issues[i].id;
+    var desc = issues[i].severity;
+    var assignedTo = issues[i].assignedTo;
+    var status = issues[i].status;
+
+    issuesList.innerHTML +='<div class="well">'+
+                           '<h6>Issue ID:'+ id +'</h6>'+
+                           '<p><span class="label label-info">' + status + '</span></p>'+
+                           '<h3>' + desc + '</h3>'+
+                           '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>'+
+                           '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>'+
+                           '<a href="#" onclick="setstatusclosed(\''+id+''\')" class="btn btn-warning">Close</a>'+
+                           '<a href="#" onclick="deleteIssue(\''+id+''\')" class="btn btn-danger">Delete</a>'+
+                           '</div>';
+
+}
+}
